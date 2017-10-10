0e0aa19e4d0c8fd0e04cf2f2a99423cb3e8b2442
menevia16a <menevia16a@gmail.com>
2017-10-11 00:20:23 +0100

    Created new C++ class UTankTrack and added a Lightmass Importance Volume

---
+++ Source/BattleTank/TankTrack.h
@@ -0,0 +1,18 @@
+// (c) Copyright 2017 Veilbreaker Entertainment
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+private:
+
+};
