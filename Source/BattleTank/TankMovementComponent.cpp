e5fbd45ceb754c805fcb5dbfa80dc5308dd12d7d
menevia16a <menevia16a@gmail.com>
2017-10-11 05:34:01 +0100

    Filled out the TankMovementComponent

--- Source/BattleTank/TankMovementComponent.cpp
+++ Source/BattleTank/TankMovementComponent.cpp
@@ -1,8 +1,20 @@
 // (c) Copyright 2017 Veilbreaker Entertainment
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
